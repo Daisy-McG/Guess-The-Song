0a16e10457e112b097037ddfc4611885eb5f0c35
ccp84 <ccp84@icloud.com>
2022-11-18 13:06:04 +0000

    api>Add creds

--- assets/js/sheetsapi.js
+++ assets/js/sheetsapi.js
@@ -1,4 +1,5 @@
 // creds
 const CLIENT_ID = 'hackathon-nov-2022';
 const API_KEY = 'AIzaSyC4lsPEu7S9fDNPYiVeKDeSJMtwg8twOws';
-const spreadsheetID = 1Ea4fV4a6Wx2zSbkBcci7ySA4JEVPvEshPTpY0z6V8zQ;
+const ACCESS_TOKEN = 'ya29.a0AeTM1idtsMmeYQhhjLqktX_20EcjanZTMF-SaybVCNtM6wPqCVlXJg1EiHcHJ4_R-_3vf3fbG35W3EXh62mc_EYG4co-LKHxrEGQ7xgnztuX0dNbpSwXP9Mg34024AmiiinsFACcRn15mGZGpamuO0OFnvz8aCgYKAfQSARISFQHWtWOmubKkJyKbwubub6k-e2VS8Q0163';
+const SPREADSHEET_ID = '1Ea4fV4a6Wx2zSbkBcci7ySA4JEVPvEshPTpY0z6V8zQ';
