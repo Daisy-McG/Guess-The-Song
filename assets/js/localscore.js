c139ab10209370de69b1b311d42b17e1399b13a4
ccp84 <ccp84@icloud.com>
2022-11-20 11:12:28 +0000

    JS>display high score

--- assets/js/localscore.js
+++ assets/js/localscore.js
@@ -1,34 +1,30 @@
 let score = String(Math.floor(Math.random() * 100));
-let username = "test2"
+let username = `test${score}`;
+
 
 let usernameButton = document.getElementById("submit_user");
 
 usernameButton.addEventListener('click', function(){
-	document.getElementById("localdata").innerHTML = "button pressed";
     console.log('button pressed');
     setScore();
 });
 
+
 function setScore() {
-    console.log(`setting username to: ${username}`);
-    console.log(`setting score to ${score}`);
-    localStorage.setItem("username",username, "score",score);
+    document.getElementById("new_score").innerHTML = `Well done ${username} you scored ${score}`;
+    localStorage.name = username;
+    localStorage.score = score; // move this to high score function
     getScore();
 }
 
-function getScore() {
-    let currentLocal = [],
-        keys = Object.keys(localStorage),
-        i = keys.length;
-    
-    while (i --) {
-        currentLocal.push(localStorage.getItem(keys[i]));
-        console.log(localStorage.getItem(keys[i]));
 
-    // console.log(localStorage.getItem("username",[i]));
-    // console.log(localStorage.getItem("score",[i]));
-    // updateUser = localStorage.getItem("username",[i]);
-    // updateScore = localStorage.getItem("username",[i]);
-    // document.getElementById("localdata").innerHTML = `Username ${updateUser} scored ${updateScore}`;
-    }
+function getScore() {
+    // let current_topuser = localStorage.getItem("username");
+    // let current_topscore = localStorage.getItem("score");
+    // console.log(localStorage.getItem("score"));
+    // document.getElementById("high_score").innerHTML = `The current high score is ${current_topuser}: ${current_topscore}`;
+
+    let topUsername = localStorage.name;
+    let topScore = localStorage.score;
+    document.getElementById("high_score").innerHTML = `The current high score is ${topUsername}: ${topScore}`;
 }
